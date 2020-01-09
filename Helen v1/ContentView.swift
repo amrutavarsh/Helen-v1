27ed54d35aacc9460ed0c56bf7aefa7125ef4cee
Amrutavarsh S Kinagi <amrutavarshk@gmail.com>
2020-01-09 18:45:17 +0530

    basic framework

--- Helen v1/ContentView.swift
+++ Helen v1/ContentView.swift
@@ -7,10 +7,44 @@
 //
 
 import SwiftUI
+import UIKit
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack{
+                VStack(alignment: .leading){
+                Text("Helen")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.leading)
+                    Text("Only clear conversations").font(.subheadline)
+                }
+                Spacer()
+            }.padding().offset(y:10)
+            
+            ZStack(alignment: .trailing){
+            CameraView().frame(height:600).offset(y:70).padding(.top, -80)
+                
+                
+                
+                Button(action:{}){
+                    Text("Start/Stop").fontWeight(.bold).padding(7)
+                    .foregroundColor(Color.white).background(Color.blue).cornerRadius(10)
+                }.offset(x:-15,y:235)
+                
+                Button(action:{}){
+                    Text("Flip cam").fontWeight(.bold).padding(7)
+                    .foregroundColor(Color.white).background(Color.black).cornerRadius(10)
+                }.offset(x:-15,y:185)
+                
+            }
+            
+            RoundedRectangle(cornerRadius:30).edgesIgnoringSafeArea(.bottom)
+            .frame(height: 250)
+         
+        }
     }
 }
 
@@ -19,3 +53,37 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CameraView : UIViewControllerRepresentable {
+    // Init ViewController
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraView>) -> UIViewController {
+        let controller = CameraViewController()
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: CameraView.UIViewControllerType, context: UIViewControllerRepresentableContext<CameraView>) {
+        
+    }
+}
+
+class CameraViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCamera()
+    }
+    
+    func loadCamera() {
+        let avSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device : captureDevice) else { return }
+        avSession.addInput(input)
+        avSession.startRunning()
+        
+        let cameraPreview = AVCaptureVideoPreviewLayer(session: avSession)
+        view.layer.addSublayer(cameraPreview)
+        cameraPreview.frame = view.frame
+    }
+}
+
