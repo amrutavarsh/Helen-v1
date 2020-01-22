9d97b092a4e9a101a7b106e713a3732ad4f2c6a6
Amrutavarsh S Kinagi <amrutavarshk@gmail.com>
2020-01-22 09:53:34 +0530

    video upload tests

--- Helen v1/CameraController.swift
+++ Helen v1/CameraController.swift
@@ -48,7 +48,7 @@ class CameraViewController : UIViewController, AVCaptureVideoDataOutputSampleBuf
     }
     
     var startStream = false
-    
+    var prevStartStream = startStream
     var frame_counter = 1
     var frameFaceFound: Bool = true
     
@@ -72,6 +72,11 @@ class CameraViewController : UIViewController, AVCaptureVideoDataOutputSampleBuf
     var cameraPreview :AVCaptureVideoPreviewLayer?
     
     let context =  CIContext()
+    let outputSize = CGSize(width: 1920, height: 1280)
+    let imagesPerSecond: TimeInterval = 0.04 //each image will be stay for 3 secs
+    var selectedPhotosArray = [UIImage]()
+    var imageArrayToVideoURL = NSURL()
+    var asset: AVAsset!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -234,28 +239,6 @@ class CameraViewController : UIViewController, AVCaptureVideoDataOutputSampleBuf
         try? imageRequestHandler.perform([faceDetectionRequest])
     }
     
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-         
-        guard let uiImage = self.imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
-        if (self.startStream){
-            //UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil);
-            self.frame_counter = self.frame_counter + 1
-            guard let data = uiImage.jpegData(compressionQuality: 1) ?? uiImage.pngData() else {return}
-            let frameKey = "fileName\(self.frame_counter).png"
-            let fileName = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(frameKey)
-            do {
-                try data.write(to: fileName)
-            } catch {   
-                print(error.localizedDescription)
-                return 
-            }
-        uploadFile(fileNameKey : frameKey, filename : fileName)
-        }
-        else{
-            startStream = false
-        }       
-    }
-    
     func imageFromSampleBuffer(sampleBuffer : CMSampleBuffer) -> UIImage? {
         guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
         self.detectFace(in: imageBuffer)
@@ -264,6 +247,106 @@ class CameraViewController : UIViewController, AVCaptureVideoDataOutputSampleBuf
         guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return nil }
         
         return UIImage(cgImage: cgImage)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+         
+        guard let uiImage = self.imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
+        if (self.startStream){
+            //UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil);
+            self.frame_counter = self.frame_counter + 1
+            selectedPhotosArray.append(UIImage(named: "\(self.frame_counter).JPG")!)
+            //guard let data = uiImage.jpegData(compressionQuality: 1) ?? uiImage.pngData() else {return}
+            //let frameKey = "fileName\(self.frame_counter).png"
+            //let fileName = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(frameKey)
+            //do {
+            //    try data.write(to: fileName)
+            //} catch {   
+            //    print(error.localizedDescription)
+            //    return 
+            //}
+            //uploadFile(fileNameKey : frameKey, filename : fileName)
+        }
+        else{
+            startStream = false
+            if prevStartStream = true{
+            buildVideoFromImageArray()
+            }    
+        }
+        prevStartStream = startStream
+    }
+    
+    func buildVideoFromImageArray() {
+        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let fileKey = "file\(frame_counter).MP4"
+        imageArrayToVideoURL = NSURL(fileURLWithPath: directory + "/" + fileKey)
+        guard let videoWriter = try? AVAssetWriter(outputURL: imageArrayToVideoURL as URL, fileType: AVFileTypeMPEG4) else {
+            fatalError("AVAssetWriter error")
+        }
+        let outputSettings = [AVVideoCodecKey : AVVideoCodecH264, AVVideoWidthKey : NSNumber(value: Float(outputSize.width)), AVVideoHeightKey : NSNumber(value: Float(outputSize.height))] as [String : Any]
+        guard videoWriter.canApply(outputSettings: outputSettings, forMediaType: AVMediaTypeVideo) else {
+            fatalError("Negative : Can't apply the Output settings...")
+        }
+        let videoWriterInput = AVAssetWriterInput(mediaType: AVMediaTypeVideo, outputSettings: outputSettings)
+        let sourcePixelBufferAttributesDictionary = [kCVPixelBufferPixelFormatTypeKey as String : NSNumber(value: kCVPixelFormatType_32ARGB), kCVPixelBufferWidthKey as String: NSNumber(value: Float(outputSize.width)), kCVPixelBufferHeightKey as String: NSNumber(value: Float(outputSize.height))]
+        let pixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: videoWriterInput, sourcePixelBufferAttributes: sourcePixelBufferAttributesDictionary)
+        if videoWriter.canAdd(videoWriterInput) {
+            videoWriter.add(videoWriterInput)
+        }
+        if videoWriter.startWriting() {
+            let zeroTime = CMTimeMake(Int64(imagesPerSecond),Int32(1))
+            videoWriter.startSession(atSourceTime: zeroTime)
+
+            assert(pixelBufferAdaptor.pixelBufferPool != nil)
+            let media_queue = DispatchQueue(label: "mediaInputQueue")
+            videoWriterInput.requestMediaDataWhenReady(on: media_queue, using: { () -> Void in
+                let fps: Int32 = 1
+                let framePerSecond: Int64 = Int64(self.imagesPerSecond)
+                let frameDuration = CMTimeMake(Int64(self.imagesPerSecond), fps)
+                var frameCount: Int64 = 0
+                var appendSucceeded = true
+                while (!self.selectedPhotosArray.isEmpty) {
+                    if (videoWriterInput.isReadyForMoreMediaData) {
+                        let nextPhoto = self.selectedPhotosArray.remove(at: 0)
+                        let lastFrameTime = CMTimeMake(frameCount * framePerSecond, fps)
+                        let presentationTime = frameCount == 0 ? lastFrameTime : CMTimeAdd(lastFrameTime, frameDuration)
+                        var pixelBuffer: CVPixelBuffer? = nil
+                        let status: CVReturn = CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, pixelBufferAdaptor.pixelBufferPool!, &pixelBuffer)
+                        if let pixelBuffer = pixelBuffer, status == 0 {
+                            let managedPixelBuffer = pixelBuffer
+                            CVPixelBufferLockBaseAddress(managedPixelBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
+                            let data = CVPixelBufferGetBaseAddress(managedPixelBuffer)
+                            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+                            let context = CGContext(data: data, width: Int(self.outputSize.width), height: Int(self.outputSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(managedPixelBuffer), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)
+                            context!.clear(CGRect(x: 0, y: 0, width: CGFloat(self.outputSize.width), height: CGFloat(self.outputSize.height)))
+                            let horizontalRatio = CGFloat(self.outputSize.width) / nextPhoto.size.width
+                            let verticalRatio = CGFloat(self.outputSize.height) / nextPhoto.size.height
+                            //let aspectRatio = max(horizontalRatio, verticalRatio) // ScaleAspectFill
+                            let aspectRatio = min(horizontalRatio, verticalRatio) // ScaleAspectFit
+                            let newSize: CGSize = CGSize(width: nextPhoto.size.width * aspectRatio, height: nextPhoto.size.height * aspectRatio)
+                            let x = newSize.width < self.outputSize.width ? (self.outputSize.width - newSize.width) / 2 : 0
+                            let y = newSize.height < self.outputSize.height ? (self.outputSize.height - newSize.height) / 2 : 0
+                            context?.draw(nextPhoto.cgImage!, in: CGRect(x: x, y: y, width: newSize.width, height: newSize.height))
+                            CVPixelBufferUnlockBaseAddress(managedPixelBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
+                            appendSucceeded = pixelBufferAdaptor.append(pixelBuffer, withPresentationTime: presentationTime)
+                        } else {
+                            print("Failed to allocate pixel buffer")
+                            appendSucceeded = false
+                        }
+                    }
+                    if !appendSucceeded {
+                        break
+                    }
+                    frameCount += 1
+                }
+                videoWriterInput.markAsFinished()
+                videoWriter.finishWriting { () -> Void in
+                    print("-----video1 url = \(self.imageArrayToVideoURL)")
+                    self.asset = AVAsset(url: self.imageArrayToVideoURL as URL)
+                }
+            })
+        }
+        uploadFile(fileNameKey: fileKey, filename: imageArrayToVideoURL)
     }
     
     func getDocumentsDirectory() -> URL {
